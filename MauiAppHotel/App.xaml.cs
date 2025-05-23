6ed31dd4831fab29367a60c2e3f2886a08e21e81
Kevim <Kevim@KEVIM_NTBK>
2025-05-22 22:06:23 -0300

    atualizações

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -1,13 +1,43 @@
 ﻿
+using MauiAppHotel.Models;
+
 namespace MauiAppHotel
 {
     public partial class App : Application
     {
+        public List<Quarto> lista_quartos = new List<Quarto>
+        {
+            new Quarto()
+            {
+                Descricao = "Suíte Super Luxo", 
+                valorDiariaAdulto = 110.0,
+                valorDiariaCrianca = 55.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Luxo",
+                valorDiariaAdulto = 80.0,
+                valorDiariaCrianca = 40.0
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Single",
+                valorDiariaAdulto = 25,
+                valorDiariaCrianca = 12.5
+            },
+            new Quarto()
+            {
+                Descricao = "Suíte Crise",
+                valorDiariaAdulto = 110.0,
+                valorDiariaCrianca = 55.0
+            }
+        };
+
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage (new Views.ContratacaoHospedagem());
 
         }
 
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Codystar-Light.ttf", "CodystarLight");
+                    fonts.AddFont("Codystar-Regular.ttf", "Codystar");
                 });
 
 #if DEBUG
